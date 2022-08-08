8afd3d2aad47d9f98595ee7c6357486b880b24f3
hunfa <83254423+hunfa@users.noreply.github.com>
2022-08-09 03:44:18 +0500

    With infinite scrolling
    with infinite scrolling

--- src/components/News.js
+++ src/components/News.js
@@ -4,86 +4,98 @@ import axios from 'axios';
 import defaultimg from '../imgnotloaded.png'
 import Spinner from './spinner';
 
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
 
-static defaultProps={
-    country:"in",
-    pageSize:8,
-    category:'general'
-}
+    static defaultProps = {
+        country: "in",
+        pageSize: 8,
+        category: 'general'
+    }
 
-constructor(){
-    super();
-    this.state={
-        articles:[],
-        totalResults:0,
-        showloading:true,
-        page:1,
-        
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            totalResults: 0,
+            page: 1,
+            hasMore: true
+
+        }
+    }
+
+    componentDidMount() {
+
+        this.fetchNews();
 
     }
-}
 
-componentDidMount(){
-   
-   this.fetchNews();
-  
-   }
-
-
- handlenext=()=>{
-        this.setState({page:this.state.page+1});
-        this.fetchNews(this.state.page+1);
-}
-
-handleprevious=()=>{
-    this.setState({page:this.state.page-1});
-    this.fetchNews(this.state.page-1);
-}
 
 
     render() {
-       
+
         return (
-            
+
             <div className='container'>
                 <h1 className='text-center my-5'>News - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
-           {this.state.showloading&& <Spinner/>}
-                <div className="row">
-                    {!this.state.showloading&& this.state.articles.map((element,index)=>{
-                        return ( <div key={index} className="col-md-4 my-3">
-                        <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage?element.urlToImage:defaultimg} url={element.url} bagde={element.source.name} publishedAt={element.publishedAt}/>
-                    </div>)
-                    })}
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.hasMore}
+                    loader={<Spinner />}
+                    scrollThreshold="90%"
+                >
+                    <div className="row">
+                        {this.state.articles.map((element, index) => {
+                            return (<div key={index} className="col-md-4 my-3">
+                                <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage ? element.urlToImage : defaultimg} url={element.url} bagde={element.source.name} publishedAt={element.publishedAt} />
+                            </div>)
+                        })}
+                    </div>
+                </InfiniteScroll>
 
-               
-               
-               
-               
-                </div>
-               {!this.state.showloading&& <div className='d-flex justify-content-between my-3'> 
-                <button onClick={this.handleprevious} disabled={this.state.page<=1} type="button" class="btn btn-dark">Previous</button>
-                    <button onClick={this.handlenext} disabled={this.state.page>Math.ceil(this.props.pageSize/this.state.page)} type="button" class="btn btn-dark">Next</button>
-               </div>}
             </div>
         )
     }
 
 
-     capitalizeFirstLetter=(string)=> {
+    capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
-      }
+    }
 
-     fetchNews=async(pageNo=this.state.page)=>{
-        this.setState({showloading:true});
-        const response=await  (await axios.get(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&pageSize=${this.props.pageSize}&page=${pageNo}&category=${this.props.category}&apiKey=b12ce90c7ca041e59e53d20736a6ce95`)).data;
-        this.setState({articles:response.articles,
-        totalResults:response.totalResults,
-        showloading:false
+    fetchNews = async (pageNo = this.state.page) => {
+        this.setState({ showloading: true });
+
+        console.log(pageNo);
+        const response = await (await axios.get(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&pageSize=${this.props.pageSize}&page=${pageNo}&category=${this.props.category}&apiKey=b12ce90c7ca041e59e53d20736a6ce95`)).data;
+        this.setState({
+            articles: response.articles,
+            totalResults: response.totalResults,
+
         });
-        // console.log(response.articles);
+
+
+
+
+    }
+
+
+    fetchMoreData = async () => {
+
+        const pageNo = this.state.page + 1;
+        this.setState({ page: this.state.page + 1 });
+        console.log(pageNo);
+        if (!(pageNo < Math.ceil(this.state.totalResults / this.props.pageSize)))
+            this.setState({ hasMore: false });
+            
         
-       }
-    
+        const response = await (await axios.get(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&pageSize=${this.props.pageSize}&page=${pageNo}&category=${this.props.category}&apiKey=b12ce90c7ca041e59e53d20736a6ce95`)).data;
+        this.setState({
+            articles: this.state.articles.concat(response.articles),
+            totalResults: response.totalResults,
+
+        });
+    };
+
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -14,9 +14,9 @@ export default class NewsItem extends Component {
           
         <img src={imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
-        <span class="badge rounded-pill text-bg-danger mb-2">{bagde}</span>
+        <span className="badge rounded-pill text-bg-danger mb-2">{bagde}</span>
         <a className='desc' href={url} target="_blank" rel="noreferrer"> <h5 className=" card-title">{title}</h5></a>
-        <p class="card-text"><small class="text-muted"><strong> {(new Date(publishedAt)).toUTCString()}</strong></small></p>
+        <p className="card-text"><small className="text-muted"><strong> {(new Date(publishedAt)).toUTCString()}</strong></small></p>
          <p className="card-text">{description}</p>
           <a href={url} target="_blank" rel="noreferrer" className="btn btn-primary">Read More</a>
         </div>
